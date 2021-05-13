77b0130924d410166f490803c90f82a6395e0f27
kocagoz <kocagoz@gmail.com>
2021-05-13 10:12:33 +0300

    first commit

---
+++ leapyear.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+
+year = int(input("Enter e Year!: "))
+
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else:
+            print(f"{year} is not a leap year")
+    else:
+        print(f"{year} is not a leap year")
+else:
+    print(f"{year} is a leap year")
